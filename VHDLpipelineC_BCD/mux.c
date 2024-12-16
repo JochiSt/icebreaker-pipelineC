f44a9363958f57e82155302f0a07e73bd26356bd
JochiSt <jochen.steinmann@web.de>
2024-12-16 16:35:28 +0100

    stick more to classic C syntax

--- VHDLpipelineC_BCD/mux.c
+++ VHDLpipelineC_BCD/mux.c
@@ -1,11 +1,14 @@
 #include "uintN_t.h"  // uintN_t types for any N
 
+#pragma MAIN_MHZ mux 100
 
-uint4_t blink(uint1_t sel, uint4_t A, uint4_t B)
+uint4_t mux(uint1_t sel, uint4_t A, uint4_t B)
 {
+    uint4_t retval = 0;
     if (sel == 0) {
-        return A;
+        retval = A;
     }else{
-        return B;
+        retval = B;
     }
+    return retval;
 }
