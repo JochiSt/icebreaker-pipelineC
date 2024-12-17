974529901ec70512a7a5db5cce9e241057baeb0f
JochiSt <jochen.steinmann@web.de>
2024-12-17 14:11:11 +0100

    added script to sort all generated VHDL files by pipelinec and to keep only the unique ones

---
+++ VHDLpipelineC_BCD/orderVHDLfiles.py
@@ -0,0 +1,46 @@
+#!/bin/python
+
+################################################################################
+# order the VHDL files, such, that they are unique and ordered
+# used to forward multiple pipelinec units within one VHDL project
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(
+                    prog='orderVHDLfiles.py',
+                    description='Order and filter the VHDL files',
+                    epilog='')
+
+parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
+parser.add_argument('-o', '--output')
+
+args = parser.parse_args()
+
+package_files = []
+built_in_files = []
+module_files = []
+main_files = []
+
+for f in args.file:
+    for line in f:
+        line = line.split()[0]    # remove \n
+        if "pkg" in line:
+            if not any(os.path.basename(line) in s for s in package_files):
+                package_files.append(line)
+        elif "built_in" in line:
+            if not any(os.path.basename(line) in s for s in built_in_files):
+                built_in_files.append(line)
+        else:
+            if not any(os.path.basename(line) in s for s in module_files):
+                module_files.append(line)
+                
+with open(args.output, "w") as outfile:
+    for file in package_files:
+        outfile.write("%s\n"%(file))
+    for file in built_in_files:
+        outfile.write("%s\n"%(file))
+    for file in module_files:
+        outfile.write("%s\n"%(file))
+    for file in main_files:
+        outfile.write("%s\n"%(file))          
