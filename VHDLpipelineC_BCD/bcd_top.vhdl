9d1a49f1b963b480a04dd21d48d11accf61cd427
JochiSt <jochen.steinmann@web.de>
2024-12-17 13:03:09 +0100

    instantiate modules form VHDL and C

--- VHDLpipelineC_BCD/bcd_top.vhdl
+++ VHDLpipelineC_BCD/bcd_top.vhdl
@@ -1,5 +1,6 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
 
 ENTITY bcd_top IS
 	GENERIC (
@@ -49,7 +50,90 @@ ARCHITECTURE behaviour OF bcd_top IS
 
 	SIGNAL dip_switches : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
 
+	COMPONENT counter IS
+		GENERIC (
+			BITWIDTH : INTEGER RANGE 0 TO 16 := 1
+		);
+		PORT (
+			clock     : IN STD_LOGIC;
+			enable    : IN STD_LOGIC;
+			direction : IN STD_LOGIC;
+			count     : OUT STD_LOGIC_VECTOR(BITWIDTH - 1 DOWNTO 0)
+		);
+
+	END COMPONENT;
+
+	COMPONENT mux IS
+		PORT (
+			clk_100p0 : IN STD_LOGIC;
+
+			-- IO for each main func
+			mux_sel           : IN unsigned(0 DOWNTO 0);
+			mux_A             : IN unsigned(3 DOWNTO 0);
+			mux_B             : IN unsigned(3 DOWNTO 0);
+			mux_return_output : OUT unsigned(3 DOWNTO 0)
+		);
+	END COMPONENT;
+	SIGNAL mux_sel : unsigned(0 DOWNTO 0) := (OTHERS => '0'); -- identical to mux_sel_slv
+	SIGNAL mux_sel_slv : STD_LOGIC_VECTOR(0 DOWNTO 0) := (OTHERS => '0'); -- identical to mux_sel
+
+	SIGNAL mux_digit : unsigned(3 DOWNTO 0) := (OTHERS => '0');
+
+	COMPONENT LUT_7segment IS
+		PORT (
+			clk_in       : IN STD_LOGIC;
+			decimal_in   : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			segments_out : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+		);
+	END COMPONENT;
+
+	SIGNAL segments : STD_LOGIC_VECTOR(6 DOWNTO 0) := (OTHERS => '0');
+
 BEGIN
 
+	digit_counter : counter
+	GENERIC MAP(
+		BITWIDTH => 1
+	)
+	PORT MAP(
+		clock     => CLK,
+		enable    => '1',
+		direction => '0',
+		count     => mux_sel_slv
+	);
+	mux_sel <= unsigned(mux_sel_slv);
+
+	digit_mux : mux
+	PORT MAP(
+		clk_100p0 => CLK,
+
+		-- IO for each main func
+		mux_sel           => mux_sel,
+		mux_A => (OTHERS => '1'),
+		mux_B => (OTHERS => '0'),
+		mux_return_output => mux_digit
+	);
+
+	LUT_7segment_0 : LUT_7segment
+	PORT MAP(
+		clk_in       => CLK,
+		decimal_in   => STD_LOGIC_VECTOR(mux_digit),
+		segments_out => segments
+	);
+
 	dip_switches <= P1B10 & P1B9 & P1B8 & P1B7 & P1B4 & P1B3 & P1B2 & P1B1;
+
+	-- assign segments to outputs
+	P1A9 <= segments(6);
+	P1A8 <= segments(5);
+	P1A7 <= segments(4);
+	P1A4 <= segments(3);
+	P1A3 <= segments(2);
+	P1A2 <= segments(1);
+	P1A1 <= segments(0);
+	-- assign anode
+	P1A10 <= mux_sel_slv(0); -- toggle between 0 and 1
+
+	LEDG_N <= mux_sel_slv(0);
+
 END behaviour; -- arch
