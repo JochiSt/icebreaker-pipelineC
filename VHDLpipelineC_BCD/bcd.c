18f4760ba4d5dc4b50a5c3ea2a31dcb4b826414a
JochiSt <jochen.steinmann@web.de>
2024-12-18 13:47:17 +0100

    started C file, which contains a BCD encoder

--- VHDLpipelineC_BCD/bcd.c
+++ VHDLpipelineC_BCD/bcd.c
@@ -1,28 +1,32 @@
-#include "uintN_t.h"  // uintN_t types for any N
+#include "uintN_t.h" // uintN_t types for any N
 
-// Count to 33333333 iterations * 30ns each ~= 1sec
-uint25_t counter;
+// 'Called'/'Executing' every 83.33ns (12MHz)
+#pragma MAIN_MHZ bcd 12
+#pragma MAIN bcd
 
-// LED on off state
-uint1_t led5;
+uint8_t bcd(uint8_t binary_in) {
 
-// 'Called'/'Executing' every 30ns (33.33MHz)
-#pragma MAIN_MHZ blink 12
-//#pragma MAIN blink
+    uint8_t bin_modify = binary_in;
+    uint8_t bcd = 0;
+    uint4_t i = 0;
+    for (i=8; i>1; i = i - 1){
 
-uint1_t blink()
-{
-  // If reached 1 second
-  if(counter==(12000000-1))
-  {
-    // Toggle led
-    led5 = !led5;
-    // Reset counter
-    counter = 0;
-  }
-  else
-  {
-    counter += 1; // one 30ns increment
-  }
-  return led5;
+        uint4_t ones = bcd & 0xF;
+        uint4_t tens = (bcd & 0xF0)>>4;
+        if (ones >= 5 ){
+            ones += 3;
+        }
+        if (tens >= 5 ){
+            tens += 3;
+        }     
+        
+        bcd = (tens << 4) | ones; 
+        //bcd = bcd << 4;
+        //bcd = bcd | ones;
+
+        bcd = bcd << 1;
+        bcd |= bin_modify & 0x1;
+        bin_modify = bin_modify >> 1;
+    }
+    return bcd;
 }
