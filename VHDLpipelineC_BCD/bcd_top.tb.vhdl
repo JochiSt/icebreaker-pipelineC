1980364b82ef41e09fd0ecd857fc6e2da57406f3
JochiSt <jochen.steinmann@web.de>
2025-01-07 10:37:39 +0100

    include setting of the dipswitches in simulation

--- VHDLpipelineC_BCD/bcd_top.tb.vhdl
+++ VHDLpipelineC_BCD/bcd_top.tb.vhdl
@@ -56,6 +56,8 @@ ARCHITECTURE simulation OF bcd_top_tb IS
 
     SIGNAL CLK : STD_LOGIC := '0';
 
+    SIGNAL dip_switches : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+
 BEGIN
 
     bcd_top_0 : bcd_top
@@ -82,14 +84,15 @@ BEGIN
         P1A9  => OPEN,
         P1A10 => OPEN,
 
-        P1B1  => '0',
-        P1B2  => '0',
-        P1B3  => '0',
-        P1B4  => '0',
-        P1B7  => '0',
-        P1B8  => '0',
-        P1B9  => '0',
-        P1B10 => '0',
+        -- P1B10 & P1B9 & P1B8 & P1B7 & P1B4 & P1B3 & P1B2 & P1B1;
+        P1B1  => dip_switches(0),
+        P1B2  => dip_switches(1),
+        P1B3  => dip_switches(2),
+        P1B4  => dip_switches(3),
+        P1B7  => dip_switches(4),
+        P1B8  => dip_switches(5),
+        P1B9  => dip_switches(6),
+        P1B10 => dip_switches(7),
 
         LED1 => OPEN,
         LED2 => OPEN,
@@ -101,13 +104,23 @@ BEGIN
         BTN3 => '0'
     );
 
-
-    clk_proc : process
-    begin
+    ----------------------------------------------------------------------------
+    clk_proc : PROCESS
+    BEGIN
         CLK <= '0';
-        wait for 10 ns;
+        WAIT FOR 10 ns;
         CLK <= '1';
-        wait for 10 ns;
-    end process ; -- clk_proc
+        WAIT FOR 10 ns;
+    END PROCESS; -- clk_proc
+    ----------------------------------------------------------------------------
+
+    dip_proc : process
+    begin
+        wait for 40 ns;
+        dip_switches <= "00000010";
+        wait for 40 ns;
+        dip_switches <= "00100000";
+        wait for 10 ms; -- wait basically until end of simulation
+    end process;
 
 END ARCHITECTURE; -- arch
