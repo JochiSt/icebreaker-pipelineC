5707c2345a1f51979bcdc8459c6fa2363ada9fad
JochiSt <jochen.steinmann@web.de>
2025-01-07 12:47:21 +0100

    make use of C syntax

--- VHDLpipelineC_BCD/pipelineC/pipelineC_bcd/bcd.C
+++ VHDLpipelineC_BCD/pipelineC/pipelineC_bcd/bcd.C
@@ -6,30 +6,11 @@
 
 uint8_t bcd(uint8_t binary_in) {
 
-    uint8_t bin_modify = binary_in;
-    uint8_t bcd = 0;
-    uint4_t i = 0;
-    for (i=8; i>1; i = i - 1){
+    uint4_t tens = binary_in / 10;
+    uint4_t ones = binary_in % 10;
 
-        // split bcd into ones and tens
-        uint4_t ones = bcd & 0xF;
-        uint4_t tens = (bcd & 0xF0)>>4;
-        if (ones >= 5 ){
-            ones += 3;
-        }
-        if (tens >= 5 ){
-            tens += 3;
-        }     
-        
-        // combine again
-        bcd = tens;
-        bcd = bcd << 4;
-        bcd = bcd | ones; 
-
-        // include the binary input
-        bcd = bcd << 1;
-        bcd |= bin_modify & 0x1;
-        bin_modify = bin_modify >> 1;
-    }
+    uint8_t bcd = tens << 4;
+    bcd = bcd | ones;
+    
     return bcd;
 }
