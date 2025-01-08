0b261664a6f867a498066b194c3ba75d42dcbf77
JochiSt <jochen.steinmann@web.de>
2025-01-08 14:55:58 +0100

    added useful tests

--- VHDLpipelineC_BCD/simulationC/pipelineC_bcd/sim_bcd.c
+++ VHDLpipelineC_BCD/simulationC/pipelineC_bcd/sim_bcd.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#include "bcd.h"
+
 int main(void){
     printf("Hello BCD test\n");
+
+    for(uint8_t binary_in = 0; binary_in < 100; binary_in++) {
+        printf("input number: %02d \t -> \t %02X\n", binary_in, bcd(binary_in) );
+    }
+
     return 0;
 }
 
