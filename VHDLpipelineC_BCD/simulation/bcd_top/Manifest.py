2c68e2b3ea7b6680827edc7ae83b1b2c7452c2a8
JochiSt <jochen.steinmann@web.de>
2025-01-08 16:08:01 +0100

    use mkdir -p to make the work folder even, when it exists

--- VHDLpipelineC_BCD/simulation/bcd_top/Manifest.py
+++ VHDLpipelineC_BCD/simulation/bcd_top/Manifest.py
@@ -6,7 +6,7 @@ sim_tool = "ghdl"
 sim_top = "bcd_top_tb"
 ghdl_opt = "--workdir=work -fsynopsys --std=08"
 
-sim_pre_cmd = "mkdir work"
+sim_pre_cmd = "mkdir -p work"
 sim_post_cmd = "ghdl -r bcd_top_tb --stop-time=5ms --wave=bcd_top_tb.ghw"
 
 incl_makefiles = [
