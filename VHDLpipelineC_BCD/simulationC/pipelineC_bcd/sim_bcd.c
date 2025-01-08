d06437796327af62b48e53c2a99c75a87a84d5ad
JochiSt <jochen.steinmann@web.de>
2025-01-08 15:09:28 +0100

    applied formatting

--- VHDLpipelineC_BCD/simulationC/pipelineC_bcd/sim_bcd.c
+++ VHDLpipelineC_BCD/simulationC/pipelineC_bcd/sim_bcd.c
@@ -2,13 +2,12 @@
 
 #include "bcd.h"
 
-int main(void){
+int main(void) {
     printf("Hello BCD test\n");
 
-    for(uint8_t binary_in = 0; binary_in < 100; binary_in++) {
-        printf("input number: %02d \t -> \t %02X\n", binary_in, bcd(binary_in) );
+    for (uint8_t binary_in = 0; binary_in < 100; binary_in++) {
+        printf("input number: %02d \t -> \t %02X\n", binary_in, bcd(binary_in));
     }
 
     return 0;
 }
-
--- VHDLpipelineC_BCD/simulationC/pipelineC_led_blink_counter/sim_led_blink_counter.c
+++ VHDLpipelineC_BCD/simulationC/pipelineC_led_blink_counter/sim_led_blink_counter.c
@@ -2,22 +2,20 @@
 
 #include "led_blink_counter.h"
 
-int main(void){
+int main(void) {
     printf("Hello LED blink counter test\n");
 
     uint1_t led = 0;
     uint1_t old_led = 0;
 
-    for(long clock_cycle = 0; clock_cycle < 50000000; clock_cycle++) {
+    for (long clock_cycle = 0; clock_cycle < 50000000; clock_cycle++) {
 
         led = led_blink_counter();
-        if(led != old_led){
+        if (led != old_led) {
             printf("LED changed at %ld to %d\n", clock_cycle, led);
         }
         old_led = led;
-
     }
 
     return 0;
 }
-
