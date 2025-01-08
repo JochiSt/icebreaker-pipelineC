2527e819be5975189aac8f997af0a52b76544f13
JochiSt <jochen.steinmann@web.de>
2025-01-08 13:06:27 +0100

    changed text to 12MHz and fixed init of led

--- VHDLpipelineC_BCD/pipelineC/pipelineC_led_blink_counter/led_blink_counter.C
+++ VHDLpipelineC_BCD/pipelineC/pipelineC_led_blink_counter/led_blink_counter.C
@@ -1,12 +1,12 @@
 #include "uintN_t.h" // uintN_t types for any N
 
-// Count to 33333333 iterations * 30ns each ~= 1sec
+// Count to 12000000 iterations * 83.2ns each ~= 1sec
 uint25_t counter;
 
 // LED on off state
-uint1_t led;
+uint1_t led = 0;
 
-// 'Called'/'Executing' every 30ns (33.33MHz)
+// 'Called'/'Executing' every 83.2ns  (12MHz)
 #pragma MAIN_MHZ led_blink_counter 12
 
 uint1_t led_blink_counter() {
