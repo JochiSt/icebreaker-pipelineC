618cfb23a968cfd241734b40cafb1e544b503047
JochiSt <jochen.steinmann@web.de>
2025-07-28 16:49:53 +0200

    starting to adapt to icebreaker board

--- top.h
+++ top.h
@@ -3,6 +3,7 @@
 
 // Get clock rate constant PLL_CLK_MHZ from header written by make flow
 #include "pipelinec_makefile_config.h"
+
 // By default PipelineC names clock ports with the rate included
 // ex. clk_12p0
 // Override this behavior by creating an input with a constant name
@@ -12,55 +13,6 @@ DECL_INPUT(uint1_t, pll_clk)
 CLK_MHZ(pll_clk, PLL_CLK_MHZ)
 DECL_INPUT(uint1_t, pll_locked)
 
-#ifdef DEFAULT_VGA_PMOD
-// Configure VGA module to use PMOD0 and PMOD1
-// rgb is 8b internally, 4b on pmod
-// PMOD0 = VGA PMOD J1
-// PMOD1 = VGA PMOD J2
-// PMOD0A for VGA pmod demo
-#define PMOD_0A_O4
-#define PMOD_0A_O3
-#define PMOD_0A_O2
-#define PMOD_0A_O1
-#define VGA_R0_WIRE pmod_0a_o1 // J1-1 = 0A IO 1
-#define VGA_R1_WIRE pmod_0a_o2 // J1-2 = 0A IO 2
-#define VGA_R2_WIRE pmod_0a_o3 // J1-3 = 0A IO 3
-#define VGA_R3_WIRE pmod_0a_o4 // J1-4 = 0A IO 4
-// PMOD0B for VGA pmod demo
-#define PMOD_0B_O4
-#define PMOD_0B_O3
-#define PMOD_0B_O2
-#define PMOD_0B_O1
-#define VGA_B0_WIRE pmod_0b_o1 // J1-7 = 0B IO 1
-#define VGA_B1_WIRE pmod_0b_o2 // J1-8 = 0B IO 2
-#define VGA_B2_WIRE pmod_0b_o3 // J1-9 = 0B IO 3
-#define VGA_B3_WIRE pmod_0b_o4 // J1-10 = 0B IO 4
-// PMOD1A for VGA pmod demo
-#define PMOD_1A_O4
-#define PMOD_1A_O3
-#define PMOD_1A_O2
-#define PMOD_1A_O1
-#define VGA_G0_WIRE pmod_1a_o1 // J2-1 = 1A IO 1
-#define VGA_G1_WIRE pmod_1a_o2 // J2-2 = 1A IO 2
-#define VGA_G2_WIRE pmod_1a_o3 // J2-3 = 1A IO 3
-#define VGA_G3_WIRE pmod_1a_o4 // J2-4 = 1A IO 4
-// PMOD1B for VGA pmod demo
-//#define PMOD_1B_O4 // unused for VGA pmod
-//#define PMOD_1B_O3 // unused for VGA pmod
-#define PMOD_1B_O2
-#define PMOD_1B_O1
-#define VGA_HS_WIRE pmod_1b_o1 // J2-7 = 1B IO 1
-#define VGA_VS_WIRE pmod_1b_o2 // J2-8 = 1B IO 2
-#endif
-
-// Board specific config
-#ifdef BOARD_PICO
-#include "board/pico_ice.h"
-#elif defined(BOARD_PICO2)
-#include "board/pico2_ice.h"
-#else
-#warning "Unknown board?"
-#endif
 
 // Instantiate UART MAC module
 #ifdef DEFAULT_PI_UART
@@ -68,14 +20,3 @@ DECL_INPUT(uint1_t, pll_locked)
 #define UART_BAUD 115200
 #include "uart/uart_mac.c"
 #endif
-
-// Instantiate VGA output wires
-#ifdef DEFAULT_VGA_PMOD
-#include "vga/vga_wires_4b.c"
-// Configure the VGA timing to use
-// 640x480 is a 25MHz pixel clock
-#define FRAME_WIDTH 640
-#define FRAME_HEIGHT 480
-#include "vga/vga_timing.h"
-#endif
-
--- top.sv
+++ top.sv
@@ -1,46 +1,72 @@
 module top(
-  // These pins should all exist in ice40.pcf
-  inout ICE_35,
-  inout ICE_39,
-  inout ICE_40,
-  inout ICE_41,
-  inout ICE_25,
-  inout ICE_27,
-  inout ICE_11,
-  inout ICE_9,
-  inout ICE_45,
-  inout ICE_47,
-  inout ICE_2,
-  inout ICE_4,
-  inout ICE_44,
-  inout ICE_46,
-  inout ICE_48,
-  inout ICE_3,
-  inout ICE_31,
-  inout ICE_34,
-  inout ICE_38,
-  inout ICE_43,
-  inout ICE_28,
-  inout ICE_32,
-  inout ICE_36,
-  inout ICE_42,
-  inout ICE_23,
-  inout ICE_26
-);
-  // ICE_35 has a default 12MHz clock in pico and pico2
+    // 12 MHz clock
+    inout CLK,
+    // RS232
+    inout RX,
+    inout TX,
+
+    // LEDs and Button
+    inout BTN_N,
+    inout LEDR_N,
+    inout LEDG_N,
+
+    // RGB LED Driver
+    inout LED_RED_N,
+    inout LED_GRN_N,
+    inout LED_BLU_N,
+
+    // SPI Flash
+    inout FLASH_SCK,
+    inout FLASH_SSB,
+    inout FLASH_IO0,
+    inout FLASH_IO1,
+    inout FLASH_IO2,
+    inout FLASH_IO3,
+
+    // PMOD 1A
+    inout P1A1 ,
+    inout P1A2 ,
+    inout P1A3 ,
+    inout P1A4 ,
+    inout P1A7 ,
+    inout P1A8 ,
+    inout P1A9 ,
+    inout P1A10,
+
+    // PMOD 1B
+    inout P1B1 ,
+    inout P1B2 ,
+    inout P1B3 ,
+    inout P1B4 ,
+    inout P1B7 ,
+    inout P1B8 ,
+    inout P1B9 ,
+    inout P1B10,
+
+    // PMOD 2
+    inout P2_1 ,
+    inout P2_2 ,
+    inout P2_3 ,
+    inout P2_4 ,
+    inout P2_7 ,
+    inout P2_8 ,
+    inout P2_9 ,
+    inout P2_10
+  );
+  // CLK has a default 12MHz clock in pico and pico2
   // PLL instance to make a clock based on 12MHz
   wire pll_clk;
   wire pll_locked;
   pll pll_inst(
-    .clock_in(ICE_35),
-    .clock_out(pll_clk),
-    .locked(pll_locked)
-  );
+        .clock_in(CLK),
+        .clock_out(pll_clk),
+        .locked(pll_locked)
+      );
 
   // PipelineC output HDL instance
   pipelinec_top pipelinec_inst(
-    // The pipelinec port names exactly match wires in this top level .sv
-    // so can use wildcard .* implicit port connection to automatically connect them
-    .*
-  );
+                  // The pipelinec port names exactly match wires in this top level .sv
+                  // so can use wildcard .* implicit port connection to automatically connect them
+                  .*
+                );
 endmodule
