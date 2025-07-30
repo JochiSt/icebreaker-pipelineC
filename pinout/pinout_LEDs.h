7451dcb55f5ea99b2f3eca7934b9cdb18ba3dd92
JochiSt <jochen.steinmann@web.de>
2025-07-30 13:06:53 +0200

    defined RGB LEDs

--- pinout/pinout_LEDs.h
+++ pinout/pinout_LEDs.h
@@ -3,7 +3,6 @@
 #include "uintN_t.h"
 #include "intN_t.h"
 
-
 // normal LEDs
 DECL_OUTPUT(uint1_t, LEDR_N)
 GLOBAL_OUT_WIRE_CONNECT(uint1_t, LEDR_N, LED1)
@@ -13,13 +12,10 @@ GLOBAL_OUT_WIRE_CONNECT(uint1_t, LEDG_N, LED2)
 
 // RGB LED
 DECL_OUTPUT(uint1_t, LED_RED_N)
+GLOBAL_OUT_WIRE_CONNECT(uint1_t, LED_RED_N, LED_RGB_R_N)
+
 DECL_OUTPUT(uint1_t, LED_GRN_N)
+GLOBAL_OUT_WIRE_CONNECT(uint1_t, LED_GRN_N, LED_RGB_G_N )
+
 DECL_OUTPUT(uint1_t, LED_BLU_N)
-
-#define LED_R_WIRE LED_RED_N
-#define LED_G_WIRE LED_GRN_N
-#define LED_B_WIRE LED_BLU_N
-
-#include "rgb_led/led_rgb_wires.c"
-
-
+GLOBAL_OUT_WIRE_CONNECT(uint1_t, LED_BLU_N, LED_RGB_B_N )
