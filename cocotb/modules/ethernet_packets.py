40530e90bc7c10b210488ebf27089b2cd663f727
JochiSt <jochen.steinmann@web.de>
2025-09-01 13:18:33 +0200

    output to PCAP seems to work

--- cocotb/modules/ethernet_packets.py
+++ cocotb/modules/ethernet_packets.py
@@ -1,3 +1,6 @@
+# the following function is heavily inspired by
+# https://www.codeproject.com/Tips/612847/Generate-a-quick-and-easy-custom-pcap-file-using-P
+
 # create UDP data to be transmitted via cocotb ethernet
 import struct
 
@@ -5,28 +8,28 @@ class Ethernet_packet(object):
     IPv4_DATAGRAM = 0x0800
     ARP = 0x0806
 
-    def __init__(self, SRC_MAC, DST_MAC):
-        self.bytes = bytearray()
-        # Ethernet Header
-        self.bytes += struct.pack("!HIHIH",
-                                  DST_MAC >> 32,
-                                  DST_MAC & 0xFFFFFFFF,
-                                  SRC_MAC >> 32,
-                                  SRC_MAC & 0xFFFFFFFF,
-                                  self.IPv4_DATAGRAM)
+    ETH_HEADER =   ('SM SM SM SM SM SM'     #Source Mac
+                    'DM DM DM DM DM DM'     #Dest Mac
+                    'PP PP')                #Protocol (0x0800 = IP)
+
+    def __init__(self, SRC_MAC, DST_MAC, protocol=IPv4_DATAGRAM):
+        self.bytes = self.ETH_HEADER
+        self.bytes = self.bytes.replace('SM SM SM SM SM SM', "%06x"%(SRC_MAC))
+        self.bytes = self.bytes.replace('DM DM DM DM DM DM', "%06x"%(DST_MAC))
+        self.bytes = self.bytes.replace('PP PP', "%04x"%(protocol))
+        print("created Ethernet Header")
+        print("\t\t" + self.bytes)
 
     def print(self):
-        for i, byte in enumerate(self.bytes):
-            if i % 4 == 0 and i > 0:
-                print("")
-            print("%02X"%(byte), end=" ")
-        print()
+        print(self.bytes)
 
     def payload(self):
-        return self.bytes
+        return bytearray(self.bytes)
 
-    # the following function is heavily inspired by
-    # https://www.codeproject.com/Tips/612847/Generate-a-quick-and-easy-custom-pcap-file-using-P
+    @staticmethod
+    def getByteLength(str1):
+        return int(len(''.join(str1.split())) / 2)
+
     def write_pcap(self, file_name):
         #Global header for pcap 2.4
         pcap_global_header =   ('D4 C3 B2 A1'
@@ -45,54 +48,68 @@ class Ethernet_packet(object):
 
         import binascii
 
-        pcap_len = len(self.bytes)
-
+        all_bytes = "".join(self.bytes.split())
+        print(all_bytes)
+        pcap_len = self.getByteLength(all_bytes)
+        print("Packet length %d"%(pcap_len))
         hex_str = "%08x"%int(pcap_len)
 
         reverse_hex_str = hex_str[6:] + hex_str[4:6] + hex_str[2:4] + hex_str[:2]
         pcaph = pcap_packet_header.replace('XX XX XX XX',reverse_hex_str)
         pcaph = pcaph.replace('YY YY YY YY',reverse_hex_str)
 
-        bytestring = pcap_global_header + pcaph + " ".join([ "%02X"%(b) for b in self.bytes ])
+        bytestring = pcap_global_header + pcaph + all_bytes
 
         bytelist = bytestring.split()
-        print(bytelist)
         bytes = binascii.a2b_hex(''.join(bytelist))
         bitout = open(file_name, 'wb')
         bitout.write(bytes)
 
 
 class IP_packet(Ethernet_packet):
+    IP_HEADER = ('45'                    # IP version and header length (multiples of 4 bytes)
+                 '00'
+                 'XX XX'                 # Length - will be calculated and replaced later
+                 '00 00'
+                 '40 00 40'
+                 '11'                    # Protocol (0x11 = UDP)
+                 'YY YY'                 # Checksum - will be calculated and replaced later
+                 'SI SI SI SI'           # Source IP (Default: 127.0.0.1)
+                 'DI DI DI DI'           # Dest IP (Default: 127.0.0.1)
+                 )
+
     def __init__(self, SRC_MAC, DST_MAC, SRC_IP, DST_IP, data_length=0, ttl=10):
         super().__init__(SRC_MAC, DST_MAC)
 
-        version_ihl = 0x45
-        TOS = 0x00
+        ip_bytes = self.IP_HEADER.replace('SI SI SI SI', "%08x"%(SRC_IP))
+        ip_bytes = ip_bytes.replace('DI DI DI DI', "%08x"%(DST_IP))
+        ip_bytes = ip_bytes.replace('XX XX' , "%04x"%( data_length + self.getByteLength(self.IP_HEADER)))
+        ip_bytes = ip_bytes.replace('YY YY' , "12 34") # invalid checksum
 
-        total_length = data_length + 5
-
-        identification = 0x00
-        flags_fragmentoffset = 0b010_000000000000
-
-        header_checksum = 0x00  # ignore header checksum
-
-        TTL = ttl     # hop count
-        protocol = 17 # UDP
-
-        # IP header
-        self.bytes += struct.pack("!BBH", version_ihl, TOS, total_length)
-        self.bytes += struct.pack("!HH", identification, flags_fragmentoffset)
-        self.bytes += struct.pack("!BBH", TTL, protocol, header_checksum)
-        self.bytes += struct.pack("!II", SRC_IP, DST_IP)
+        self.bytes += ip_bytes
+        print("... added IP header")
+        print("\t\t" + ip_bytes)
 
 class UDP_packet(IP_packet):
+    UDP_HEADER = (
+                'SP SP'                 # Source Port
+                'DP DP'                 # Destination Port - will be replaced later
+                'LL LL'                 # Length - will be calculated and replaced later
+                '00 00')
 
 
     def __init__(self, SRC_MAC, DST_MAC, SRC_IP, DST_IP, SRC_PORT, DST_PORT, data):
-        super().__init__(SRC_MAC, DST_MAC, SRC_IP, DST_IP, len(data))
-        # UDP header
-        self.bytes += struct.pack("!HHHH", SRC_PORT, DST_PORT, len(data), 0x00)
+        udp_length = Ethernet_packet.getByteLength(data) + Ethernet_packet.getByteLength(self.UDP_HEADER)
 
+        super().__init__(SRC_MAC, DST_MAC, SRC_IP, DST_IP, udp_length)
+
+        udp_bytes = self.UDP_HEADER.replace("SP SP", "%04x"%(SRC_PORT))
+        udp_bytes = udp_bytes.replace("DP DP", "%04x"%(DST_PORT))
+        udp_bytes = udp_bytes.replace("LL LL", "%04x"%(udp_length) )
+
+        self.bytes += udp_bytes + data
+        print("... added UDP header")
+        print("\t\t" + udp_bytes)
 
 
 if __name__ == "__main__":
@@ -105,7 +122,7 @@ if __name__ == "__main__":
         192_168_1_5,
         0x1234,
         0x1234,
-        "test1234"
+        "12 34 56 78"
         )
 
     udp_packet.print()
