3455300d1b8fa3569251a3cfdc4c8d26c56a4924
JochiSt <jochen.steinmann@web.de>
2025-10-15 14:55:33 +0200

    added some byte swaps and included proper ARP OPcode for reply.

--- VHDLpipelineC_Ethernet/arp_reply/arp.h
+++ VHDLpipelineC_Ethernet/arp_reply/arp.h
@@ -3,8 +3,7 @@
 // This describes the arp to be done:
 // 	Input data format
 // 	Output data format
-// 	The actual computation 'arp' to be done
-// Ideally in PipelineC/C code that can be compiled+checked in software easily.
+
 #pragma once
 
 #include "intN_t.h"
@@ -15,15 +14,29 @@
 
 #define ETHERTYPE_ARP       0x0806
 
-// MATRIX MULT EXAMPLE
-#define DIM 2
-#define data_t int8_t
+#define ARP_HARDWARE_TYPE   0x0100  // Hardware type (e.g., Ethernet)   0x0001  bytes swapped
+#define ARP_PROTOCOL_TYPE   0x0008  // Protocol type (e.g., IPv4)       0x0800  bytes swapped
+#define ARP_HARDWARE_SIZE   6       // Size of hardware address (MAC address size)
+#define ARP_PROTOCOL_SIZE   4       // Size of protocol address (IPv4 address size)
+
+#define ARP_OPCODE_REQUEST  0x0100       // ARP request 1   bytes swapped
+#define ARP_OPCODE_REPLY    0x0200       // ARP reply   2   bytes swapped
 
 typedef struct arp_request_t
 {
-  data_t matrix0[DIM][DIM];
-  data_t matrix1[DIM][DIM];
+    uint16_t hardware_type;         // Hardware type
+    uint16_t protocol_type;         // Protocol type
+    uint8_t hardware_size;          // Length of hardware address
+    uint8_t protocol_size;          // Length of protocol address
+    uint16_t operation;             // Operation: request or reply
+    uint48_t sender_hardware_addr;  // Sender MAC address
+    uint32_t sender_protocol_addr;  // Sender IP address
+    uint48_t target_hardware_addr;  // Target MAC address
+    uint32_t target_protocol_addr;  // Target IP address
 } arp_request_t;
+
+// TODO: maybe there is a nicer way, than copying the struct here again. But at
+//          least, this does work with PipelineC
 
 #ifdef __PIPELINEC__
     #include "arp_request_t_bytes_t.h"
@@ -34,7 +47,15 @@ typedef struct arp_request_t
 
 typedef struct arp_reply_t
 {
-  data_t result[DIM][DIM];
+    uint16_t hardware_type;         // Hardware type
+    uint16_t protocol_type;         // Protocol type
+    uint8_t hardware_size;          // Length of hardware address
+    uint8_t protocol_size;          // Length of protocol address
+    uint16_t operation;             // Operation: request or reply
+    uint48_t sender_hardware_addr;  // Sender MAC address
+    uint32_t sender_protocol_addr;  // Sender IP address
+    uint48_t target_hardware_addr;  // Target MAC address
+    uint32_t target_protocol_addr;  // Target IP address
 } arp_reply_t;
 
 #ifdef __PIPELINEC__
@@ -44,23 +65,24 @@ typedef struct arp_reply_t
     #include "type_bytes_t.h/arp_reply_t_bytes_t.h/arp_reply_t_bytes.h"
 #endif
 
-// Basic PipelineC implementation without pointers
+// build the arp reply packet based on the request data
+// TODO: need to check the target IP address in the request
 arp_reply_t arp(arp_request_t req)
 {
     arp_reply_t reply;
-    uint32_t i;
-    uint32_t j;
-    uint32_t k;
-    for (i = 0; i < DIM; i+=1)
-    {
-        for (j = 0; j < DIM; j+=1)
-        {
-            reply.result[i][j] = 0;
-            for (k = 0; k < DIM; k+=1)
-            {
-                reply.result[i][j] += req.matrix0[i][k] * req.matrix1[k][j];
-            }
-        }
-    }
+
+    reply.hardware_type = ARP_HARDWARE_TYPE;
+    reply.protocol_type = ARP_PROTOCOL_TYPE;
+    reply.hardware_size = ARP_HARDWARE_SIZE;
+    reply.protocol_size = ARP_PROTOCOL_SIZE;
+
+    reply.operation = ARP_OPCODE_REPLY;
+
+    reply.target_hardware_addr = req.sender_hardware_addr;
+    reply.target_protocol_addr = req.sender_protocol_addr;
+
+    reply.sender_hardware_addr = FPGA_MAC;
+    reply.sender_protocol_addr = FPGA_IP;
+
     return reply;
 }
