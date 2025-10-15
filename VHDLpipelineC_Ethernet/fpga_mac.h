396131367ba79ca3ac632308e35dd285f323c22b
JochiSt <jochen.steinmann@web.de>
2025-10-15 14:12:12 +0200

    added FPGA IP address

--- VHDLpipelineC_Ethernet/fpga_mac.h
+++ VHDLpipelineC_Ethernet/fpga_mac.h
@@ -15,6 +15,14 @@
 uint8_t FPGA_MAC_BYTES[6] = {FPGA_MAC0, FPGA_MAC1, FPGA_MAC2, FPGA_MAC3, FPGA_MAC4, FPGA_MAC5};
 #define FPGA_MAC uint8_array6_be(FPGA_MAC_BYTES)
 
+// FPGA IP address
+#define FPGA_IP0    192
+#define FPGA_IP1    168
+#define FPGA_IP2    0
+#define FPGA_IP3    20
+uint8_t FPGA_IP_BYTES[4] = {FPGA_IP0, FPGA_IP1, FPGA_IP2, FPGA_IP3};
+#define FPGA_IP uint8_array4_be(FPGA_IP_BYTES)
+
 ////////////////////////////////////////////////////////////////////////////////
 // Broadcast MAC address
 #define BROADCAST_MAC0   0xFF
