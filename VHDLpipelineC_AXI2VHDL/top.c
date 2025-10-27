deaa163f24e44e171fcd4cc8e2611e564ce1a9d2
JochiSt <jochen.steinmann@web.de>
2025-10-27 15:43:33 +0100

    added formatting and some math

--- VHDLpipelineC_AXI2VHDL/top.c
+++ VHDLpipelineC_AXI2VHDL/top.c
@@ -6,11 +6,11 @@
 // AXI building blocks are a work in progress
 // see ex. axi/axis.h
 // But for now just define a little demo 'my' type here
-typedef struct my_axis_32_t{
-  uint8_t data[4];
-  uint1_t keep[4];
-  uint1_t last;
-}my_axis_32_t;
+typedef struct my_axis_32_t {
+    uint8_t data[4];
+    uint1_t keep[4];
+    uint1_t last;
+} my_axis_32_t;
 
 // declare stream and handshakes
 DECL_STREAM_TYPE(my_axis_32_t)
@@ -19,28 +19,29 @@ DECL_HANDSHAKE_INST_TYPE(my_axis_32_t, my_axis_32_t) // out type, in type
 
 // Single cycle stateful(static locals) function (a state machine)
 // for working with an input and output handshake
-hs_out(my_axis_32_t) my_func( hs_in(my_axis_32_t) inputs ){
+hs_out(my_axis_32_t) my_func(hs_in(my_axis_32_t) inputs) {
 
-  hs_out(my_axis_32_t) outputs; // Default value all zeros
+    hs_out(my_axis_32_t) outputs; // Default value all zeros
 
-  // Static = register
-  static stream(my_axis_32_t) buff;
+    // Static = register
+    static stream(my_axis_32_t) buff;
 
     outputs.stream_out = buff;
     outputs.ready_for_stream_in = ~buff.valid;
 
-  // Input ready writes buffer
-  if(outputs.ready_for_stream_in){
-    buff = inputs.stream_in;
-  }
+    // Input ready writes buffer
+    if (outputs.ready_for_stream_in) {
+        buff = inputs.stream_in;
 
-  // Output ready clears buffer
-  // and switches to next buffer
-  if(inputs.ready_for_stream_out){
-    buff.valid = 0;
-  }
+        buff.data.data[0] += 1;
+    }
 
-  return outputs;
+    // Output ready clears buffer
+    if (inputs.ready_for_stream_out) {
+        buff.valid = 0;
+    }
+
+    return outputs;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -60,37 +61,37 @@ DECL_INPUT(uint1_t, m_axis_tready)
 
 // Demo connecting two my_func's back to back
 #pragma MAIN_MHZ top 100.0
-void top(){
-  // Handshake style code with helper macros etc
-  // Requires instantiating the modules to be used first at start of func
-  // func0: my_axis_32_t my_func(my_axis_32_t)
-  DECL_HANDSHAKE_INST(func0, my_axis_32_t, my_func, my_axis_32_t)
-  // func1: my_axis_32_t my_func(my_axis_32_t)
-  DECL_HANDSHAKE_INST(func1, my_axis_32_t, my_func, my_axis_32_t)
+void top() {
+    // Handshake style code with helper macros etc
+    // Requires instantiating the modules to be used first at start of func
+    // func0: my_axis_32_t my_func(my_axis_32_t)
+    DECL_HANDSHAKE_INST(func0, my_axis_32_t, my_func, my_axis_32_t)
+    // func1: my_axis_32_t my_func(my_axis_32_t)
+    DECL_HANDSHAKE_INST(func1, my_axis_32_t, my_func, my_axis_32_t)
 
-  // Connect flattened top level input ports to local stream variables
-  stream(my_axis_32_t) input_axis;
-  UINT_TO_BYTE_ARRAY(input_axis.data.data, 4, s_axis_tdata)
-  UINT_TO_BIT_ARRAY(input_axis.data.keep, 4, s_axis_tkeep)
-  input_axis.data.last = s_axis_tlast;
-  input_axis.valid = s_axis_tvalid;
+    // Connect flattened top level input ports to local stream variables
+    stream(my_axis_32_t) input_axis;
+    UINT_TO_BYTE_ARRAY(input_axis.data.data, 4, s_axis_tdata)
+    UINT_TO_BIT_ARRAY(input_axis.data.keep, 4, s_axis_tkeep)
+    input_axis.data.last = s_axis_tlast;
+    input_axis.valid = s_axis_tvalid;
 
-  // Input stream into first instance
-  // func0 input handshake = input_axis, s_axis_tready
-  HANDSHAKE_FROM_STREAM(func0, input_axis, s_axis_tready)
+    // Input stream into first instance
+    // func0 input handshake = input_axis, s_axis_tready
+    HANDSHAKE_FROM_STREAM(func0, input_axis, s_axis_tready)
 
-  // Output of first instance into second
-  // func1 input handshake = func0 output handshake
-  HANDSHAKE_CONNECT(func1, func0)
+    // Output of first instance into second
+    // func1 input handshake = func0 output handshake
+    HANDSHAKE_CONNECT(func1, func0)
 
-  // Output stream from second instance
-  stream(my_axis_32_t) output_axis;
-  // output_axis, m_axis_tready = func1 output handshake
-  STREAM_FROM_HANDSHAKE(output_axis, m_axis_tready, func1)
+    // Output stream from second instance
+    stream(my_axis_32_t) output_axis;
+    // output_axis, m_axis_tready = func1 output handshake
+    STREAM_FROM_HANDSHAKE(output_axis, m_axis_tready, func1)
 
-  // Connect flattened top level output ports from local stream type variables
-  m_axis_tdata = uint8_array4_le(output_axis.data.data); // Array to uint
-  m_axis_tkeep = uint1_array4_le(output_axis.data.keep); // Array to uint
-  m_axis_tlast = output_axis.data.last;
-  m_axis_tvalid = output_axis.valid;
+    // Connect flattened top level output ports from local stream type variables
+    m_axis_tdata = uint8_array4_le(output_axis.data.data); // Array to uint
+    m_axis_tkeep = uint1_array4_le(output_axis.data.keep); // Array to uint
+    m_axis_tlast = output_axis.data.last;
+    m_axis_tvalid = output_axis.valid;
 }
